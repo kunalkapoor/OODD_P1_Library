d2b475b953226c828c556da42d7be8e62336f377
Kunal Kapoor <kkapoor@ncsu.edu>
2016-09-25 23:43:32 -0400

    production environment changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: "25",
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "ncsulibrarysystem",
+      password: "ncsu27606"
+  }
+
+  config.action_mailer.default_url_options = { host: "oodd-library.herokuapp.com"}
+
+
 end
